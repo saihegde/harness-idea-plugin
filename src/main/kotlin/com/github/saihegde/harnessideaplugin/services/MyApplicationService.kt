1d1e0632bfc48e7e3d0a77aa8607f717b4e33aaa
GitHub Action <action@github.com>
2022-07-22 20:49:22 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/saihegde/harnessideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.saihegde.harnessideaplugin.services
+
+import com.github.saihegde.harnessideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/saihegde/harnessideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/saihegde/harnessideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.saihegde.harnessideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.saihegde.harnessideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
